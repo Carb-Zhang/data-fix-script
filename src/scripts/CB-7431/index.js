9270283c79ee377970ab4d1b0b7128f41117ccce
Carb <carb.zhang@storehub.com>
2024-02-07 11:35:48 +0800

    chore

--- src/scripts/CB-7431/index.js
+++ src/scripts/CB-7431/index.js
@@ -3,9 +3,8 @@ import { Types } from 'mongoose';
 
 const business = 'outletsawarimenarafelda';
 const registerId = new Types.ObjectId('654b2fee06a87f0006020a4a');
-const oldProductId = new Types.ObjectId('654b309ecc081a0007e78845');
-const newProductId = new Types.ObjectId('655ef1257b9bd000070a9283');
-
+const oldProductId = '654b309ecc081a0007e78845';
+const newProductId = '655ef1257b9bd000070a9283';
 const receiptNumbers = [
     '0012312051115138',
     '0012312051209422',
@@ -17,10 +16,16 @@ const receiptNumbers = [
     '0012312221426505',
 ];
 
+// const business = 'onlytestaccount';
+// const registerId = new Types.ObjectId('5f2b68b9da32e6000667c424');
+// const oldProductId = '604747731684a40006ae188e';
+// const newProductId = '656956dfc4675e00076acb98';
+
+// const receiptNumbers = ['00000000153'];
+
 async function fixOneOrder(order) {
     order.items.forEach((item) => {
-        console.log(item.productId);
-        if (item.productId === oldProductId) {
+        if (item.productId && item.productId.toString() === oldProductId) {
             console.log(item);
         }
     });
