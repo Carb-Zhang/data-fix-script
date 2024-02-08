9179a7e67868c0c91951b461309e597a27993f6c
Carb <carb.zhang@storehub.com>
2024-02-08 17:21:26 +0800

    chore

--- src/scripts/CB-7523/index.js
+++ src/scripts/CB-7523/index.js
@@ -3,9 +3,8 @@ import { Types } from 'mongoose';
 const ObjectId = Types.ObjectId;
 
 export async function run() {
-    const startId = ObjectId.createFromTime(
-        new Date('2024-02-01T00:00:00.000+08:00').getTime() / 1000,
-    );
+    const startTime = Math.floor(Date.now() / 1000) - 7 * 24 * 3600;
+    const startId = ObjectId.createFromTime(startTime);
     const count = await ThirdPartyOnlineOrder.default.count({ _id: { $gt: startId }, channel: 12 });
     console.log(count);
 }
