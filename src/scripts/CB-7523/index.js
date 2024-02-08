3eacdace789316ed578e263c08e65ad7ae8cef0f
Carb <carb.zhang@storehub.com>
2024-02-08 17:27:21 +0800

    chore

--- src/scripts/CB-7523/index.js
+++ src/scripts/CB-7523/index.js
@@ -1,10 +1,13 @@
 import ThirdPartyOnlineOrder from '../../models/thirdPartyOnlineOrder.js';
 import { Types } from 'mongoose';
 const ObjectId = Types.ObjectId;
+import { writeFileSync } from 'fs';
 
 export async function run() {
     const startTime = Math.floor(Date.now() / 1000) - 7 * 24 * 3600;
     const startId = ObjectId.createFromTime(startTime);
-    const count = await ThirdPartyOnlineOrder.default.count({ _id: { $gt: startId }, channel: 12 });
-    console.log(count);
+    const filter = { _id: { $gt: startId }, channel: 12 };
+    // const count = await ThirdPartyOnlineOrder.default.count({ _id: { $gt: startId }, channel: 12 });
+    const orders = await ThirdPartyOnlineOrder.default.find(filter).lean();
+    writeFileSync('fp_orders.json', JSON.stringify(orders));
 }
