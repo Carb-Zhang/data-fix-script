af4eca3c89c1c85d1524001c81d7d2eb80206e2a
Carb <carb.zhang@storehub.com>
2024-02-09 16:23:48 +0800

    chore

--- src/scripts/CB-7523/index.js
+++ src/scripts/CB-7523/index.js
@@ -1,14 +1,43 @@
 import ThirdPartyOnlineOrder from '../../models/thirdPartyOnlineOrder.js';
 import { Types } from 'mongoose';
 const ObjectId = Types.ObjectId;
-import { writeFileSync } from 'fs';
+import { createWriteStream } from 'fs';
+import { sleep } from '../../utils/tools.js';
+
+const writeStream = createWriteStream('fp_orders.json');
+
+function writeOneDoc(str) {
+    return new Promise((res, rej) => {
+        writeStream.write(str, (err) => {
+            if (err) {
+                console.log('err', err);
+                rej(err);
+            } else {
+                res();
+            }
+        });
+    });
+}
 
 export async function run() {
     const startTime = Math.floor(Date.now() / 1000) - 365 * 24 * 3600;
     const startId = ObjectId.createFromTime(startTime);
     const filter = { _id: { $gt: startId }, channel: 12 };
-    // const count = await ThirdPartyOnlineOrder.default.count(filter);
-    // console.log(count);
-    const orders = await ThirdPartyOnlineOrder.default.find(filter).lean();
-    writeFileSync('fp_orders.json', JSON.stringify(orders));
+    let isFirst = true;
+    await ThirdPartyOnlineOrder.default
+        .find(filter)
+        .lean()
+        .cursor()
+        .eachAsync(async (doc) => {
+            var prefix = isFirst ? '[' : ',';
+            if (isFirst) {
+                isFirst = false;
+            }
+
+            let docToReturn = prefix + JSON.stringify(doc);
+            await writeOneDoc(docToReturn);
+        });
+    await writeOneDoc(']');
+    writeStream.end(() => console.log('导出完成'));
+    await sleep(1000);
 }
