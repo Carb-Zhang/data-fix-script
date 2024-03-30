783dfa59f938af3f120eee83008959ca1e42cd42
Carb <carb.zhang@storehub.com>
2024-03-30 15:58:11 +0800

    CB-7821

---
+++ src/scripts/CB-7821/index.js
@@ -0,0 +1,174 @@
+import InventoryChangeEventCollection from '../../models/inventoryChangeEvent.js';
+import InventoryCollection from '../../models/inventory.js';
+import {
+    InventoryChangeEventStatus,
+    InventoryChangeEventType,
+} from '../../models/inventoryChangeEvent.js';
+
+import _ from 'lodash';
+
+const oneDayInMS = 24 * 3600 * 1000;
+
+async function getRecentChangedInvs() {
+    const businesses = await InventoryChangeEventCollection.default.distinct('business', {
+        createdAt: {
+            $gt: new Date(Date.now() - oneDayInMS * 30),
+            $lt: new Date(Date.now() - oneDayInMS * 29),
+        },
+        eventType: InventoryChangeEventType.ORDER_OFFLINE_PLACE,
+    });
+    const events = await InventoryChangeEventCollection.default
+        .find({
+            createdAt: {
+                $gt: new Date(Date.now() - oneDayInMS),
+            },
+            status: InventoryChangeEventStatus.SUCCESS,
+            business: { $in: businesses },
+        })
+        .select({ storeId: 1, 'updates.productId': 1, 'updates.storeId': 1 })
+        .lean();
+    const invs = [];
+    events.forEach((event) => {
+        event.updates.forEach(({ storeId, productId }) => {
+            invs.push({
+                storeId: storeId || event.storeId,
+                productId: productId,
+            });
+        });
+    });
+    return invs.length > 50 ? invs.slice(0, 9) : invs;
+}
+
+async function getInventoryChanges(params) {
+    const { storeId, productId, startAt, endAt } = params;
+    const setChanges = await InventoryChangeEventCollection.default.aggregate([
+        {
+            $match: {
+                storeId: '',
+                updates: {
+                    $elemMatch: {
+                        storeId,
+                        productId,
+                    },
+                },
+                status: 'SUCCESS',
+                eventStartAt: {
+                    $gte: startAt,
+                    $lt: endAt,
+                },
+            },
+        },
+        {
+            $project: {
+                eventType: 1,
+                updates: {
+                    $filter: {
+                        input: '$updates',
+                        as: 'update',
+                        cond: {
+                            $and: [
+                                { $eq: ['$$update.storeId', storeId] },
+                                { $eq: ['$$update.productId', productId] },
+                            ],
+                        },
+                    },
+                },
+            },
+        },
+        {
+            $unwind: '$updates',
+        },
+        {
+            $project: {
+                eventType: 1,
+                quantityBefore: '$updates.amountBefore',
+                quantityAfter: '$updates.amountAfter',
+                quantityDiff: '$updates.updateAmount',
+                changedAt: '$updates.updatedAt',
+            },
+        },
+    ]);
+    const incChanges = await InventoryChangeEventCollection.default.aggregate([
+        {
+            $match: {
+                storeId,
+                'updates.productId': productId,
+                status: 'SUCCESS',
+                eventStartAt: {
+                    $gte: startAt,
+                    $lt: endAt,
+                },
+            },
+        },
+        {
+            $project: {
+                eventType: 1,
+                updates: {
+                    $filter: {
+                        input: '$updates',
+                        as: 'update',
+                        cond: {
+                            $and: [{ $eq: ['$$update.productId', productId] }],
+                        },
+                    },
+                },
+            },
+        },
+        {
+            $unwind: '$updates',
+        },
+        {
+            $project: {
+                eventType: 1,
+                quantityBefore: '$updates.amountBefore',
+                quantityAfter: '$updates.amountAfter',
+                quantityDiff: '$updates.updateAmount',
+                changedAt: '$updates.updatedAt',
+            },
+        },
+    ]);
+    return _.reverse(_.sortBy([...setChanges, ...incChanges], ['changedAt']));
+}
+
+function checking(auditTrails, currentQty) {
+    let i = 0;
+    let currentRecord = auditTrails[i];
+    if (currentRecord.quantityAfter !== currentQty) {
+        console.log(`currentQty not consistent`);
+    }
+    let lastRecord = null;
+
+    while (++i < auditTrails.length) {
+        lastRecord = auditTrails[i];
+        if (currentRecord.quantityBefore !== lastRecord.quantityAfter) {
+            console.log(`audit trail not consistent, ${JSON.stringify(currentRecord)}`);
+        }
+        currentRecord = lastRecord;
+    }
+}
+
+async function verifyInv(inv) {
+    const { storeId, productId } = inv;
+    console.log(`Start verify ${storeId} ${productId}`);
+
+    const res = await InventoryCollection.find(inv);
+    if (!res[0]) {
+        return;
+    }
+    const auditTrails = await getInventoryChanges({
+        storeId,
+        productId,
+        startAt: new Date(Date.now() - oneDayInMS * 30),
+        endAt: new Date(Date.now() + oneDayInMS),
+    });
+    checking(auditTrails, res[0].quantityOnHand);
+}
+
+export async function run() {
+    const invs = await getRecentChangedInvs();
+    console.log(`Get ${invs.length} waiting verify invs`);
+    let i = -1;
+    while (++i < invs.length) {
+        await verifyInv(invs[i]);
+    }
+}
