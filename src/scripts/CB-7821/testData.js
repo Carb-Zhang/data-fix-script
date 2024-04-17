74758c5bfec6fe55975c9bff178451a26ab404e9
Carb <carb.zhang@storehub.com>
2024-04-17 12:21:01 +0800

    chore

--- src/scripts/CB-7821/testData.js
+++ src/scripts/CB-7821/testData.js
@@ -35,3 +35,55 @@ export const testRecords = [
         changedAt: new Date('2024-03-31T00:01:00.000Z'),
     },
 ];
+
+export const testRecordsWithInc = [
+    {
+        eventType: 'ORDER_OFFLINE_PLACE',
+        quantityBefore: 12,
+        quantityAfter: 11,
+        quantityDiff: -1,
+        changedAt: new Date('2024-03-31T00:03:00.000Z'),
+    },
+    {
+        eventType: 'ORDER_OFFLINE_PLACE',
+        quantityBefore: 4,
+        quantityAfter: 14,
+        quantityDiff: 10,
+        changedAt: new Date('2024-03-31T00:02:00.000Z'),
+    },
+    {
+        eventType: 'ORDER_OFFLINE_PLACE',
+        quantityBefore: 13,
+        quantityAfter: 12,
+        quantityDiff: -1,
+        changedAt: new Date('2024-03-31T00:02:00.000Z'),
+    },
+    {
+        eventType: 'ORDER_OFFLINE_PLACE',
+        quantityBefore: 14,
+        quantityAfter: 13,
+        quantityDiff: -1,
+        changedAt: new Date('2024-03-31T00:02:00.000Z'),
+    },
+    {
+        eventType: 'ORDER_OFFLINE_PLACE',
+        quantityBefore: 5,
+        quantityAfter: 4,
+        quantityDiff: -1,
+        changedAt: new Date('2024-03-31T00:02:00.000Z'),
+    },
+    {
+        eventType: 'ORDER_OFFLINE_PLACE',
+        quantityBefore: 6,
+        quantityAfter: 5,
+        quantityDiff: -1,
+        changedAt: new Date('2024-03-31T00:02:00.000Z'),
+    },
+    {
+        eventType: 'ORDER_OFFLINE_PLACE',
+        quantityBefore: 7,
+        quantityAfter: 6,
+        quantityDiff: -1,
+        changedAt: new Date('2024-03-31T00:02:00.000Z'),
+    },
+];
