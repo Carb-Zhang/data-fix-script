3521e7d647cb2a5ecac0332ad704f4ce26550a74
Carb <carb.zhang@storehub.com>
2024-04-17 11:22:54 +0800

    chore

---
+++ src/scripts/CB-7821/sortAuditTrails.js
@@ -0,0 +1,66 @@
+import _ from 'lodash';
+import { DateTime } from 'luxon';
+import { testRecords } from './testData.js';
+
+function isEqual(v1, v2) {
+    return Math.abs(v1 - v2) < 0.1;
+}
+
+function timeGapInSeconds(date1, date2) {
+    return DateTime.fromJSDate(date1).diff(DateTime.fromJSDate(date2), 'seconds').as('seconds');
+}
+
+function getNextRecordGroup(records, startIndex) {
+    let i = startIndex;
+
+    const nextRecordGroup = [records[i]];
+    let currentRecord = null;
+    let nextRecord = null;
+    while (records[i + 1]) {
+        currentRecord = records[i];
+        nextRecord = records[i + 1];
+        const timeGap = timeGapInSeconds(currentRecord.changedAt, nextRecord.changedAt);
+        i++;
+        if (timeGap < 2) {
+            nextRecordGroup.push(nextRecord);
+        } else {
+            break;
+        }
+    }
+    return nextRecordGroup;
+}
+
+function sortNextRecordGroup(records, startIndex) {
+    const nextRecordGroup = getNextRecordGroup(records, startIndex);
+    const isMonotonic = !nextRecordGroup.find(({ diff }) => diff > 0);
+    if (isMonotonic) {
+        return _.sortBy(nextRecordGroup, ['quantityAfter']);
+    } else {
+        // todo
+    }
+}
+
+function sortAuditTrails(originRecords) {
+    if (originRecords.length === 0) {
+        return;
+    }
+    let pivot = 0;
+    let currentRecord = null;
+    let nextRecord = null;
+    const sortedRecords = [originRecords[0]];
+    while (originRecords[pivot + 1]) {
+        currentRecord = originRecords[pivot];
+        nextRecord = originRecords[pivot + 1];
+        if (isEqual(currentRecord.quantityBefore, nextRecord.quantityAfter)) {
+            sortedRecords.push(nextRecord);
+            pivot++;
+        } else {
+            const sortedNextRecordGroup = sortNextRecordGroup(originRecords, pivot + 1);
+            sortedRecords.push(...sortedNextRecordGroup);
+            pivot += sortedNextRecordGroup.length;
+        }
+    }
+    return sortedRecords;
+}
+
+console.log(sortAuditTrails(testRecords));
---
+++ src/scripts/CB-7821/testData.js
@@ -0,0 +1,37 @@
+export const testRecords = [
+    {
+        eventType: 'ORDER_OFFLINE_PLACE',
+        quantityBefore: 1,
+        quantityAfter: 0,
+        quantityDiff: -1,
+        changedAt: new Date('2024-03-31T00:01:00.000Z'),
+    },
+    {
+        eventType: 'ORDER_OFFLINE_PLACE',
+        quantityBefore: 4,
+        quantityAfter: 3,
+        quantityDiff: -1,
+        changedAt: new Date('2024-03-31T00:02:00.000Z'),
+    },
+    {
+        eventType: 'ORDER_OFFLINE_PLACE',
+        quantityBefore: 2,
+        quantityAfter: 1,
+        quantityDiff: -1,
+        changedAt: new Date('2024-03-31T00:02:00.000Z'),
+    },
+    {
+        eventType: 'ORDER_OFFLINE_PLACE',
+        quantityBefore: 3,
+        quantityAfter: 2,
+        quantityDiff: -1,
+        changedAt: new Date('2024-03-31T00:02:00.000Z'),
+    },
+    {
+        eventType: 'ORDER_OFFLINE_PLACE',
+        quantityBefore: 5,
+        quantityAfter: 4,
+        quantityDiff: -1,
+        changedAt: new Date('2024-03-31T00:03:00.000Z'),
+    },
+];
