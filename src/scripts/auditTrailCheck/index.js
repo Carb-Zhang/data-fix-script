685b1c440192eeb258c53d9c2d246bdb6237d8e0
Carb <carb.zhang@storehub.com>
2024-08-02 14:23:59 +0800

    audittrailcheck-1

--- src/scripts/auditTrailCheck/index.js
+++ src/scripts/auditTrailCheck/index.js
@@ -3,11 +3,11 @@ import TransactionRecord from '../../models/transactionRecord.js';
 import { createWriteStream } from 'fs';
 import { sleep } from '../../utils/tools.js';
 
-const writeStream = createWriteStream('orders_with_fail_inv_message.json');
+const writeOrderStream = createWriteStream('orders_with_fail_inv_message.json');
 
-function writeOneDoc(str) {
+function writeOneOrderDoc(str) {
     return new Promise((res, rej) => {
-        writeStream.write(str, (err) => {
+        writeOrderStream.write(str, (err) => {
             if (err) {
                 console.log('err', err);
                 rej(err);
@@ -18,7 +18,22 @@ function writeOneDoc(str) {
     });
 }
 
-const BUSINESS = 'paporma';
+const writeEventStream = createWriteStream('fail_event.json');
+
+function writeOneEventDoc(str) {
+    return new Promise((res, rej) => {
+        writeEventStream.write(str, (err) => {
+            if (err) {
+                console.log('err', err);
+                rej(err);
+            } else {
+                res();
+            }
+        });
+    });
+}
+
+const BUSINESS = 'onlytestaccount';
 
 async function checkOrdersWithFailMessage() {
     const filter = {
@@ -29,7 +44,6 @@ async function checkOrdersWithFailMessage() {
 
     let isFirst = true;
     await TransactionRecord.find(filter)
-        .sort({ createdAt: -1 })
         .select({ _id: 1 })
         .lean()
         .cursor()
@@ -41,13 +55,46 @@ async function checkOrdersWithFailMessage() {
             }
 
             let docToReturn = prefix + JSON.stringify(doc._id.toString());
-            await writeOneDoc(docToReturn);
+            await writeOneOrderDoc(docToReturn);
         });
-    await writeOneDoc(']');
-    writeStream.end(() => console.log('导出完成'));
+    await writeOneOrderDoc(']');
+    writeOrderStream.end(() => console.log('导出完成'));
+    await sleep(1000);
+}
+
+async function checkEvent() {
+    const filter = {
+        business: BUSINESS,
+        status: { $ne: 'SUCCESS' },
+        eventType: { $ne: 'ORDER_ONLINE_BEEP_PAY_FIRST_PLACE' },
+        'updates.0': { $exists: true },
+        isNeedManualCheck: true,
+        createdAt: { $gt: new Date('2024-03-25T10:36:23.940+08:00') },
+    };
+
+    let isFirst = true;
+    await InventoryChangeEvent.default
+        .find(filter)
+        .sort({ createdAt: -1 })
+        .select({ updates: 0 })
+        .lean()
+        .cursor()
+        .addCursorFlag('noCursorTimeout', true)
+        .eachAsync(async (doc) => {
+            var prefix = isFirst ? '[' : ',';
+            if (isFirst) {
+                isFirst = false;
+            }
+
+            let docToReturn = prefix + JSON.stringify(doc);
+            await writeOneEventDoc(docToReturn);
+        });
+    await writeOneEventDoc(']');
+    writeEventStream.end(() => console.log('导出完成'));
     await sleep(1000);
 }
 
 export async function run() {
     await checkOrdersWithFailMessage();
+    await checkEvent();
 }
