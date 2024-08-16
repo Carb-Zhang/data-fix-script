6e8a9fcf2ff4e0863139bcdea70b68db8c3b5742
Carb <carb.zhang@storehub.com>
2024-08-16 17:09:11 +0800

    test

--- src/scripts/auditTrail/index.js
+++ src/scripts/auditTrail/index.js
@@ -2,7 +2,7 @@ import { checkEvent } from './checkFailEvents.js';
 import { checkOrdersWithFailMessage } from './checkLostMessages.js';
 
 // const BUSINESS = 'paporma';
-const START_TIME = new Date('2023-08-16T13:36:23.940+08:00');
+const START_TIME = new Date('2024-08-16T13:36:23.940+08:00');
 
 export async function run() {
     await checkOrdersWithFailMessage('', START_TIME);
