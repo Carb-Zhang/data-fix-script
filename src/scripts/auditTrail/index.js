26acbe4ad9e711bda8cc0266a0ef73e8ad358cf4
Carb <carb.zhang@storehub.com>
2024-09-03 11:21:31 +0800

    update

--- src/scripts/auditTrail/index.js
+++ src/scripts/auditTrail/index.js
@@ -8,6 +8,6 @@ const END_TIME = new Date('2024-08-16T14:55:23.940+08:00');
 
 export async function run() {
     // await checkOrdersWithFailMessage('', START_TIME, END_TIME);
-    // await checkEvent('', START_TIME);
-    await test();
+    await checkEvent('', START_TIME);
+    // await test();
 }
