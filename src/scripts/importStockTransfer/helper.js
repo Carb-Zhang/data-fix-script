7a5d69f90defddb10e69f9f9fdc7f618b468dee8
Carb <carb.zhang@storehub.com>
2024-09-05 23:44:21 +0800

    update

--- src/scripts/importStockTransfer/helper.js
+++ src/scripts/importStockTransfer/helper.js
@@ -2,28 +2,53 @@ import { Types } from 'mongoose';
 import kue from 'kue';
 const ObjectId = Types.ObjectId;
 import Product from '../../models/product.js';
+import SerialNumber from '../../models/serialNumber.js';
 
 function getProductByIdOrSKU(businessName, identifier, callback) {
-    var BusinessProducts = Product;
     var query;
     if (ObjectId.isValid(identifier)) {
         query = {
-            $or: [{ _id: ObjectId(identifier) }, { business: businessName, skuNumber: identifier }],
+            $or: [
+                { _id: new ObjectId(identifier) },
+                { business: businessName, skuNumber: identifier },
+            ],
         };
     } else {
         query = { skuNumber: identifier };
     }
 
-    BusinessProducts.find(query)
+    Product.find(query)
         .where('isDeleted')
         .ne(true)
         .exec()
-        .then((res) => callback(null, res))
+        .then((res) => {
+            callback(null, res);
+        })
         .catch((err) => callback(err));
 }
 
 export const productHelper = {
     getProductByIdOrSKU,
+};
+
+export const serialNumberHelper = {
+    getSerialNumbersByFilter: async (businessName, filter) => {
+        const { storeId, productSerialNums } = filter;
+        
+        const queryFilter = {};
+        if (storeId) {
+          Object.assign(queryFilter, { storeId })
+        }
+        if (productSerialNums) {
+          Object.assign(queryFilter, { $or: productSerialNums })
+        }
+        return await SerialNumber.find({
+          storeId,
+          $or: productSerialNums,
+        })
+          .select({ productId: 1, serialNum: 1 })
+          .lean()
+      },
 };
 
 export const awsS3Helper = {
@@ -33,6 +58,15 @@ export const awsS3Helper = {
     uploadFile: function (filePath, resultFilePath, callback) {
         callback();
     },
+};
+
+export const stockTransferHelper = {
+    StockTransferStatus: {
+        CREATED: 'created',
+        SHIPPED: 'shipped',
+        COMPLETED: 'completed',
+        CANCELLED: 'cancelled',
+      }
 };
 export const kueHelper = {
     createQueue: function () {
--- src/scripts/importStockTransfer/index.js
+++ src/scripts/importStockTransfer/index.js
@@ -1,30 +1,36 @@
-import _ from 'lodash';
 import fs from 'fs';
 import { promisify } from 'util';
 import csv from 'csv';
 import StockTransfer from '../../models/stockTransfer.js';
-import { productHelper, i18n, awsS3Helper, kueHelper } from './helper.js';
+import {
+    productHelper,
+    i18n,
+    awsS3Helper,
+    kueHelper,
+    stockTransferHelper,
+    serialNumberHelper,
+} from './helper.js';
 const jobs = kueHelper.createQueue();
+import _ from 'lodash';
 import { sleep } from '../../utils/tools.js';
 
-async function parseOneRow(row, business) {
-    const skuNumber = row['SKU'];
-    const productId = row['Product Id'];
-    const orderedQty = parseFloat(row['Ordered Qty']);
+const { StockTransferStatus } = stockTransferHelper;
 
-    if (!skuNumber && !productId) {
+async function parseImportStockTransferRow(row, business, sourceStoreId) {
+    const identifier = row['Product Id'] || row['SKU'];
+    const orderedQty = parseFloat(row['Ordered Qty']);
+    const rawSerialNumbers = row['Serial Numbers'] ? row['Serial Numbers'].split(';') : null;
+
+    if (!identifier) {
         throw new Error('StockManagement.Valid SKU or Product Id entry is required');
     }
-    if (!orderedQty || isNaN(orderedQty)) {
+    if ((!orderedQty || isNaN(orderedQty)) && !rawSerialNumbers) {
         throw new Error(
             "StockManagement.The 'Ordered Qty' entry is required note only numerical values allowed",
         );
     }
-
-    const product = await promisify(productHelper.getProductByIdOrSKU)(
-        business,
-        skuNumber || productId,
-    )[0];
+    const products = await promisify(productHelper.getProductByIdOrSKU)(business, identifier);
+    const product = products[0];
     if (!product) {
         throw new Error('StockManagement.SKU or Product Id is incorrect');
     }
@@ -34,21 +40,54 @@ async function parseOneRow(row, business) {
         );
     }
 
-    if (product.isSerialized) {
+    const productId = product._id.toString();
+    if (!product.isSerialized) {
+        return {
+            productId,
+            quantity: orderedQty,
+            isSerialized: false,
+            serialNumbers: [],
+        };
+    }
+
+    // check serial numbers
+    if (!rawSerialNumbers) {
+        throw new Error('No serial numbers detected.');
+    }
+
+    const serialNumbers = Array.from(new Set(rawSerialNumbers));
+    const existSerialNums = await serialNumberHelper.getSerialNumbersByFilter(business, {
+        storeId: sourceStoreId,
+        productSerialNums: serialNumbers.map((serialNumber) => ({
+            productId,
+            serialNum: serialNumber,
+        })),
+    });
+    const nonExistSerialNumbers = _.difference(serialNumbers, _.map(existSerialNums, 'serialNum'));
+    if (nonExistSerialNumbers.length > 0) {
         throw new Error(
-            'StockManagement.This product cannot be added because it is using Serialised inventory type',
+            `The serial number(s) ${nonExistSerialNumbers.join(
+                ';',
+            )} not found. Please ensure the correct serial number(s) provided.`,
         );
     }
 
     return {
-        productId: product._id.toString(),
-        quantity: orderedQty,
+        productId,
+        quantity: serialNumbers.length,
+        isSerialized: true,
+        serialNumbers,
     };
 }
 
-// 1. csv -> csv with error,
-// 2. edited orderItems
-function parseCSV(business, locale, inputFilePath, outputFilePath, outputStream, callback) {
+function parseImportStockTransferCsv(
+    business,
+    sourceStoreId,
+    locale,
+    inputFilePath,
+    outputStream,
+    callback,
+) {
     const editedOrderedItems = [];
     let isCsvValid = true;
 
@@ -61,18 +100,18 @@ function parseCSV(business, locale, inputFilePath, outputFilePath, outputStream,
         })
         .to(outputStream, { header: false, newColumns: true, end: true, eof: true })
         .transform(
-            (row, index, transformCallback) => {
-                parseOneRow(row, business)
+            (row, _, transformCallback) => {
+                parseImportStockTransferRow(row, business, sourceStoreId)
                     .then((editedOrderedItem) => {
                         editedOrderedItems.push(editedOrderedItem);
                         transformCallback(null);
                     })
                     .catch((err) => {
                         isCsvValid = false;
-                        console.log(err);
+                        console.log('ParseOneRow Error', err);
                         row['Error'] = (err.message || '').startsWith('StockManagement')
                             ? i18n.__({
-                                  phrase: 'StockManagement.This product cannot be added because it is using Serialised inventory type',
+                                  phrase: err.message,
                                   locale,
                               })
                             : err.message;
@@ -86,84 +125,95 @@ function parseCSV(business, locale, inputFilePath, outputFilePath, outputStream,
         })
         .on('error', function (err) {
             outputStream.end();
-            callback(err, { editedOrderedItems, isCsvValid });
+            callback(err);
         });
 }
 
 async function processImportStockTransferJob(jobData) {
     const { business, stockTransferId, locale, file: rawFilePath } = jobData;
+    const LOCAL_OUTPUT_FOLDER = 'src/scripts/importStockTransfer/output/';
+    const REMOTE_OUTPUT_FOLDER = '/stocktransfers/import/results/';
+
     console.log(
         `Start importing stock Transfer, business ${business}, stockTransferId: ${stockTransferId}, file: ${rawFilePath}`,
     );
 
     // check stock transfer
     const stockTransfer = await StockTransfer.findById(stockTransferId);
-
-    // download file
-    await promisify(awsS3Helper.downloadFile)(rawFilePath);
+    if (!stockTransfer) {
+        throw new Error('Stock transfer not exists');
+    }
+    if (stockTransfer.status !== StockTransferStatus.CREATED) {
+        throw new Error(`Cannot edit current stock transfer, status: ${stockTransfer.status}`);
+    }
 
     // create output file
-    const outputFilePath = 'jobResults/stocktransfers/import/' + rawFilePath.split('/').pop();
+    const fileName = rawFilePath.split('/').pop();
+    const outputFilePath = LOCAL_OUTPUT_FOLDER + fileName;
     const outputStream = fs.createWriteStream(outputFilePath);
-    const headers = ['Product Name', 'SKU', 'Product Id', 'Ordered Qty', 'Error'];
+    const headers = ['Product Name', 'SKU', 'Product Id', 'Ordered Qty', 'Serial Numbers', 'Error'];
     outputStream.write(headers.join(',') + '\n');
 
     // parse raw file
-    const { editedOrderedItems, isCsvValid } = await promisify(parseCSV)(
+    await promisify(awsS3Helper.downloadFile)(rawFilePath);
+    const { editedOrderedItems, isCsvValid } = await promisify(parseImportStockTransferCsv)(
         business,
+        stockTransfer.sourceStoreId,
         locale,
         rawFilePath,
-        outputFilePath,
         outputStream,
     );
+    fs.unlinkSync(rawFilePath);
     outputStream.end();
 
-    // save changes in db
-    if (isCsvValid) {
-        const editedProductIds = editedOrderedItems.map(({ productId }) => productId);
-        stockTransfer.orderedItems = stockTransfer.orderedItems.filter(
-            ({ productId }) => !editedProductIds.includes(productId),
-        );
-        stockTransfer.orderedItems.push(...editedOrderedItems);
-        await stockTransfer.save();
-        return '';
+    if (!isCsvValid) {
+        // upload and return output file
+        await promisify(awsS3Helper.uploadFile)(outputFilePath, outputFilePath);
+        fs.unlinkSync(outputFilePath);
+        return REMOTE_OUTPUT_FOLDER + fileName;
     }
 
-    // return output file
-    await promisify(awsS3Helper.uploadFile)(outputFilePath, outputFilePath);
-    fs.unlinkSync(rawFilePath);
+    // save changes in db
     fs.unlinkSync(outputFilePath);
-
-    return outputFilePath;
+    const editedProductIds = editedOrderedItems.map(({ productId }) => productId);
+    stockTransfer.orderedItems = stockTransfer.orderedItems.filter(
+        ({ productId }) => !editedProductIds.includes(productId),
+    );
+    stockTransfer.orderedItems.push(...editedOrderedItems);
+    await stockTransfer.save();
+    return '';
 }
 
 jobs.process('Import stocktransfers', (job, done) => {
     processImportStockTransferJob(job.data)
-        .then((res) => {
-            console.log(res);
-            done(res);
+        .then((outputFilePath) => {
+            done(null, outputFilePath);
         })
         .catch((err) => {
-            console.log(err);
+            console.log('Handle Import stocktransfers Error', err);
             done(err);
         });
 });
 
 export async function run() {
+    fs.copyFileSync(
+        'src/scripts/importStockTransfer/raw/1.csv',
+        'src/scripts/importStockTransfer/raw/1 copy.csv',
+    );
     const importJob = jobs.create('Import stocktransfers', {
         business: 'onlytestaccount',
         stockTransferId: '66d597d89338005f1409805c',
         locale: '',
-        file: '1.csv',
+        file: 'src/scripts/importStockTransfer/raw/1 copy.csv',
     });
     importJob.save(function (err) {
         if (err) {
             console.log(err);
         } else {
             importJob
-                .on('complete', console.log)
-                .on('failed attempt', console.log)
-                .on('failed', console.log);
+                .on('complete', (res) => console.log('complete', res))
+                .on('failed attempt', (res) => console.log('failed attempt', res))
+                .on('failed', (res) => console.log('failed', res));
         }
     });
     await sleep(10 * 60 * 1000);
