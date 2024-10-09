a5fa148fc0c08f7a946bc7a1b949dfb72c699465
Carb <carb.zhang@storehub.com>
2024-10-09 18:44:30 +0800

    update

--- src/scripts/eInvoice/index.js
+++ src/scripts/eInvoice/index.js
@@ -1,6 +1,10 @@
-import { resetOrdersEInvoiceInfo } from './resetOrdersEInvoiceInfo.js';
+// import { resetOrdersEInvoiceInfo } from './resetOrdersEInvoiceInfo.js';
+import { DateTime } from 'luxon';
 
 export async function run() {
     // await prepareData();
-    await resetOrdersEInvoiceInfo();
+    // await resetOrdersEInvoiceInfo();
+    const time1 = DateTime.now().setZone('UTC+8').startOf('month').minus({ months: 1 }).toJSDate();
+    const time2 = DateTime.now().startOf('month').minus({ months: 1 }).toJSDate();
+    console.log(time1, time2);
 }
