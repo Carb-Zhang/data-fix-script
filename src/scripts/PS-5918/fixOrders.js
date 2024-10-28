887a2e938a01a8e4874ad4045806a751a9e686a9
Carb <carb.zhang@storehub.com>
2024-10-29 01:37:33 +0800

    update

---
+++ src/scripts/PS-5918/fixOrders.js
@@ -0,0 +1,17 @@
+import { parseCsv } from '../../utils/index.js';
+import TransactionRecord from '../../models/transactionRecord.js';
+
+export async function fixOrders() {
+    const orders = await parseCsv('src/scripts/PS-5918/records/oldOrders.csv', true);
+    for (let i = 0; i < orders.length; i++) {
+        const { business, receiptNumber, modifiedTime, isCancelled } = orders[i];
+        if (isCancelled !== 'true') {
+            await TransactionRecord.updateOne(
+                { business, receiptNumber },
+                { $set: { createdTime: new Date(modifiedTime) } },
+            );
+        }
+    }
+}
+
+// manual handle cancel orders
--- src/scripts/PS-5918/index.js
+++ src/scripts/PS-5918/index.js
@@ -2,9 +2,10 @@ import { locateScope } from './locateScope.js';
 import { prepareData } from './mockData.js';
 import { checkCancelOrdersInZReading } from './checkCancelOrders.js';
 import { backUp } from './backUp.js';
+import { fixOrders } from './fixOrders.js';
 
 export async function run() {
     // await prepareData();
     // await locateScope();
-    await backUp();
+    await fixOrders();
 }
