f7f5d3486815c42b7de8f053b3394015289652b8
carb <carb.zhang@storehub.com>
2025-01-15 15:37:29 +0800

    update

--- src/scripts/eInvoice/mockConsolidateOrderForTest.js
+++ src/scripts/eInvoice/mockConsolidateOrderForTest.js
@@ -5,7 +5,7 @@ import EInvoiceRequestRecord from '../../models/eInvoiceRequestRecord.js';
 const business = 'myuatstore';
 
 export async function mockConsolidateOrderForTest(isRevert) {
-    const receiptNumbers = ['853092210264389'];
+    const receiptNumbers = ['853092702968910'];
 
     const documentType = isRevert ? 'INVOICE' : 'CONSOLIDATE_INVOICE';
     const recordUpdateInfo = { $set: { eInvoiceDocumentType: documentType } };
