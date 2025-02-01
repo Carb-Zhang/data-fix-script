2cd875550792d766f1e75b3e3947822bdbc50d9f
carb <carb.zhang@storehub.com>
2025-02-02 01:47:42 +0800

    update

---
+++ src/scripts/202502/index.js
@@ -0,0 +1,29 @@
+import EInvoiceConsolidationTask from '../../models/eInvoiceConsolidationTask.js';
+import Business from '../../models/business.js';
+import _ from 'lodash';
+
+export async function run() {
+    const merchants = await Business.find({
+        isEInvoiceEnabled: true,
+    })
+        .select({
+            name: 1,
+            'stores._id': 1,
+            'stores.eInvoiceSetting.isEInvoiceReady': 1,
+            'stores.eInvoiceSetting.isConsolidationOn': 1,
+        })
+        .lean();
+    const storeIds = [];
+    merchants.forEach((m) => {
+        m.stores.forEach((s) => {
+            if (
+                _.get(s, 'eInvoiceSetting.isEInvoiceReady') &&
+                _.get(s, 'eInvoiceSetting.isConsolidationOn')
+            ) {
+                storeIds.push(s._id.toString());
+            }
+        });
+    });
+    console.log(storeIds);
+    console.log('count', storeIds.length);
+}
